41afce2653fc7089db9f43c7f3f89b2af8e7f1d7
Paloma Gladine <paloma.gladine@gmail.com>
2023-04-24 20:37:20 +0200

    feat: router, first pages and login component

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,11 +1,30 @@
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ShoppingListPage from './pages/ShoppingListPage'
+import TaskListPage from './pages/TaskListPage'
 
 
 function App() {
-  
+
+
   return (
-    <>
-      Hola
-    </>
+    <div className="App h-screen bg-lightGreen">
+
+      <Routes>
+
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/home" element={<HomePage />} />
+
+        <Route path="/shopping-list" element={<ShoppingListPage />} />
+
+        <Route path="/task-list" element={<TaskListPage />} />
+
+      </Routes>
+
+
+    </div>
   )
 }
 
