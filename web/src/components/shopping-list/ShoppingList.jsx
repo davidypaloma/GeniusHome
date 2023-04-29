ce1b157d3be6e99324a10718b3675e967b8ce9e5
Paloma Gladine <paloma.gladine@gmail.com>
2023-04-29 11:25:29 +0200

    feat: components structure

---
+++ web/src/components/shopping-list/ShoppingList.jsx
@@ -0,0 +1,103 @@
+// import React from 'react'
+
+function ShoppingList() {
+  return (
+    <>
+      <p className="opacity-40 italic text-end mb-4 mr-4">Last update: 19 / 04 /2023</p>
+      <div className="grid lg:grid-cols-3 lg:gap-x-16 lg:gap-y-10 md:grid-1 md:gap-6 -place-content-center">
+
+        <div className="bg-darkGreen w-64 h-44 rounded-[2rem] p-4 flex items-center">
+          <div className="border-2 border-yellow-500 h-full w-1/2 rounded-xl"></div>
+          <div className="h-full w-1/2 rounded-xl flex flex-col justify-center pl-1">
+            <p>Naranjas</p>
+            <p>(Mercadona)</p>
+            <p>2Kg</p>
+            <button className="p-1 w-5/6  bg-lightRed rounded-lg mx-auto">Delete</button>
+          </div>
+        </div>
+
+        <div className="bg-darkGreen w-64 h-44 rounded-[2rem] p-4 flex items-center">
+          <div className="border-2 border-yellow-500 h-full w-1/2 rounded-xl"></div>
+          <div className="h-full w-1/2 rounded-xl flex flex-col justify-center pl-1">
+            <p>Naranjas</p>
+            <p>(Mercadona)</p>
+            <p>2Kg</p>
+            <button className="p-1 w-5/6  bg-lightRed rounded-lg mx-auto">Delete</button>
+          </div>
+        </div>
+
+        <div className="bg-darkGreen w-64 h-44 rounded-[2rem] p-4 flex items-center">
+          <div className="border-2 border-yellow-500 h-full w-1/2 rounded-xl"></div>
+          <div className="h-full w-1/2 rounded-xl flex flex-col justify-center pl-1">
+            <p>Naranjas</p>
+            <p>(Mercadona)</p>
+            <p>2Kg</p>
+            <button className="p-1 w-5/6  bg-lightRed rounded-lg mx-auto">Delete</button>
+          </div>
+        </div>
+
+        <div className="bg-darkGreen w-64 h-44 rounded-[2rem] p-4 flex items-center">
+          <div className="border-2 border-yellow-500 h-full w-1/2 rounded-xl"></div>
+          <div className="h-full w-1/2 rounded-xl flex flex-col justify-center pl-1">
+            <p>Naranjas</p>
+            <p>(Mercadona)</p>
+            <p>2Kg</p>
+            <button className="p-1 w-5/6  bg-lightRed rounded-lg mx-auto">Delete</button>
+          </div>
+        </div>
+
+        <div className="bg-darkGreen w-64 h-44 rounded-[2rem] p-4 flex items-center">
+          <div className="border-2 border-yellow-500 h-full w-1/2 rounded-xl"></div>
+          <div className="h-full w-1/2 rounded-xl flex flex-col justify-center pl-1">
+            <p>Naranjas</p>
+            <p>(Mercadona)</p>
+            <p>2Kg</p>
+            <button className="p-1 w-5/6  bg-lightRed rounded-lg mx-auto">Delete</button>
+          </div>
+        </div>
+
+        <div className="bg-darkGreen w-64 h-44 rounded-[2rem] p-4 flex items-center">
+          <div className="border-2 border-yellow-500 h-full w-1/2 rounded-xl"></div>
+          <div className="h-full w-1/2 rounded-xl flex flex-col justify-center pl-1">
+            <p>Naranjas</p>
+            <p>(Mercadona)</p>
+            <p>2Kg</p>
+            <button className="p-1 w-5/6  bg-lightRed rounded-lg mx-auto">Delete</button>
+          </div>
+        </div>
+
+        <div className="bg-darkGreen w-64 h-44 rounded-[2rem] p-4 flex items-center">
+          <div className="border-2 border-yellow-500 h-full w-1/2 rounded-xl"></div>
+          <div className="h-full w-1/2 rounded-xl flex flex-col justify-center pl-1">
+            <p>Naranjas</p>
+            <p>(Mercadona)</p>
+            <p>2Kg</p>
+            <button className="p-1 w-5/6  bg-lightRed rounded-lg mx-auto">Delete</button>
+          </div>
+        </div>
+
+        <div className="bg-darkGreen w-64 h-44 rounded-[2rem] p-4 flex items-center">
+          <div className="border-2 border-yellow-500 h-full w-1/2 rounded-xl"></div>
+          <div className="h-full w-1/2 rounded-xl flex flex-col justify-center pl-1">
+            <p>Naranjas</p>
+            <p>(Mercadona)</p>
+            <p>2Kg</p>
+            <button className="p-1 w-5/6  bg-lightRed rounded-lg mx-auto">Delete</button>
+          </div>
+        </div>
+        <div className="bg-darkGreen w-64 h-44 rounded-[2rem] p-4 flex items-center">
+          <div className="border-2 border-yellow-500 h-full w-1/2 rounded-xl"></div>
+          <div className="h-full w-1/2 rounded-xl flex flex-col justify-center pl-1">
+            <p>Naranjas</p>
+            <p>(Mercadona)</p>
+            <p>2Kg</p>
+            <button className="p-1 w-5/6  bg-lightRed rounded-lg mx-auto">Delete</button>
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default ShoppingList
