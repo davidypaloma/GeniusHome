b74967046605d43077483fddd03fefeb2a2b55fb
Paloma Gladine <paloma.gladine@gmail.com>
2023-04-29 10:24:29 +0200

    feat: cookies configuration detail

--- api/config/cors.config.js
+++ api/config/cors.config.js
@@ -1,5 +1,6 @@
 const cors = require('cors');
 
 module.exports = cors({
-  origin: process.env.CORS_ORIGIN ||'http://localhost:3000'
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  credentials: true
 })
