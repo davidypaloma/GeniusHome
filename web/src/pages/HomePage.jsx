c9a1645a11618522bd887a346ff80c6a47d91e43
Paloma Gladine <paloma.gladine@gmail.com>
2023-04-30 14:12:36 +0200

    feat: dates configuration

--- web/src/pages/HomePage.jsx
+++ web/src/pages/HomePage.jsx
@@ -3,15 +3,18 @@ import PageLayout from "@/components/layout/PageLayout";
 import LargeWidget from "@/components/widgets/LargeWidget";
 import SmallWidget from "@/components/widgets/SmallWidget";
 import cleaningTaskService from '@/services/cleaning-tasks'
+import { format } from "date-fns";
 
 function HomePage() {
   const [cleaningTasks, setCleaningTasks] = useState([])
+  const [lastUpdate, setLastUpdate] = useState("")
 
   useEffect(() => {
     cleaningTaskService.list()
       .then((cleaningTasksResponse) => {
         console.log(cleaningTasksResponse);
         setCleaningTasks(cleaningTasksResponse)
+        setLastUpdate(format(new Date(cleaningTasksResponse[0].updatedAt), 'dd/MM/yyyy'))
       })
       .catch(console.error)
   }, [])
@@ -21,11 +24,11 @@ function HomePage() {
 
         <div className="grid grid-rows-[repeat(3,1fr)]">
 
-          <LargeWidget title="Shopping list" date="19/04/2023">
+          <LargeWidget title="Shopping list" date={lastUpdate}>
             {/* children */}
           </LargeWidget>
 
-          <LargeWidget title="Cleaning tasks" date="21/04/2023">
+          <LargeWidget title="Cleaning tasks" date={lastUpdate}>
             <div className="mt-8 text-start">
               {cleaningTasks.map((cleaningTask) => (
                 <div key={cleaningTask.id}>{cleaningTask.name}</div>
