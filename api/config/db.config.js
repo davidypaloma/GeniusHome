47adf1f63bed5ff14c57ccdb6413594ea6d311f7
David Reguart <reguartdavid@gmail.com>
2023-05-02 22:09:34 +0200

    feat: Cleaning Task list - delete task at home page

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/geniushome';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/geniushome';
 
 mongoose.connect(MONGODB_URI)
   .then(() => console.info('Successfully connect to the database'))
